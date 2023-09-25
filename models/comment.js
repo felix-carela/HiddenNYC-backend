ccf2c5d60e1a2e4210cf8188be03f9d03f1cede4
Naischasuriel <naishasuriel24@gmail.com>
2023-09-25 14:26:26 -0400

    update models

---
+++ models/comment.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema({
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  content: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1,
+    maxlength: 1000
+  },
+  post: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Post',
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date
+  }
+});
+
+export default mongoose.model('Comment', commentSchema);
