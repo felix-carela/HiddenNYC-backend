372d393061a46d99f3f33329167cab2bd06e07d0
Naischasuriel <naishasuriel24@gmail.com>
2023-09-26 13:52:19 -0400

    updated comments in controllers also the event and likes in the models

--- models/event.js
+++ models/event.js
@@ -5,10 +5,52 @@ const eventSchema = new mongoose.Schema({
     ref: 'User',
     type: mongoose.Schema.Types.ObjectId,
   },
-  address: String,
+  eventName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  address: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  coordinates: {
+    latitude: {
+      type: Number,
+      required: true,
+      min: -90,
+      max: 90
+    },
+    longitude: {
+      type: Number,
+      required: true,
+      min: -180,
+      max: 180
+    }
+  },
   category: {
-    enum: ['Music', 'Sports', 'Food', 'Arts', 'Other']
-  }
+    type: String,
+    enum: ['Music', 'Sports', 'Food', 'Arts', 'Other'],
+    required: true
+  },
+  likes: {
+    type: Number,
+    default: 0
+  },
+  dislikes: {
+    type: Number,
+    default: 0
+  },
+  //tracks which users have liked or disliked an event. ensure that they can only like an event once
+  usersLiked: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  usersDisliked: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }]
 })
 
-export default mongoose.model('Event', eventSchema)
+export default mongoose.model('Event', eventSchema);
