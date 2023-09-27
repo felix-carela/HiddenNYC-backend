62d06fcbbed89d27755a25570a64fafa63b10165
felix-carela <felixacarela@protonmail.com>
2023-09-27 12:07:48 -0400

    Implement getEvents function

--- routes/events.js
+++ routes/events.js
@@ -4,7 +4,7 @@ import * as eventController from '../controllers/events.js'
 
 const router = Router()
 
-router.get('/event', eventController.getEvents)
+router.get('/', eventController.getEvents)
 router.post('/:id', eventController.createEvent)
 router.delete('/:id', eventController.deleteEvent)
 
